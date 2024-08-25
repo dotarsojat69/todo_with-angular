25bc2743c2cd5a72a7c12ebdfd216afac5901bc5
dotarsojat69 <tdeath457@gmail.com>
2024-08-25 18:44:09 +0700

    feat: refactor

--- todo_with_angular/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { TodoFormComponent } from './components/todo-form/todo-form.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: '/todos', pathMatch: 'full' },
-  { path: 'todos', component: TodoListComponent },
-  { path: 'add-todo', component: TodoFormComponent },
-];
