f572f4eddef44d32937d8ebdbab99733826723a2
dotarsojat69 <tdeath457@gmail.com>
2024-08-25 18:46:31 +0700

    first init

---
+++ todo_with-angular/src/main.ts
@@ -0,0 +1,10 @@
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule)
+  ]
+}).catch(err => console.error(err));
